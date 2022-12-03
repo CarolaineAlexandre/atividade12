75aa804f425825832ab08d5c5a2d924a6b43e9b9
CarolaineAlexandre <carolaine.ferrazemp@gmail.com>
2022-12-03 10:43:40 -0300

    segunda parte

--- js/script.js
+++ js/script.js
@@ -10,15 +10,74 @@ function consultaCep(){
 
     request.onload = ()=> {
         var response = JSON.parse(request.responseText);
-
         if(response.erro === true){
             document.getElementById('return').innerHTML = 'CEP NÃO ENCONTRADO';
         } else{
             document.getElementById('return').innerHTML = 'CEP: ' + response.cep + '<br>' +
                                                         'Lougradouro: ' + response.lougadouro + '<br>' +                                              'Bairro: ' + response.bairro + '<br>' +
                                                         'Localidade: ' + response.localidade + '/' + response.uf;
-        }           
+        }      
+             
     }
 
 request.send();
 }
+
+let listaCep = [
+    { id: 1, tarefa: '',feita: false },
+    { id: 2, tarefa: '', feita: false },
+    { id: 3, tarefa: '', feita: false }
+];
+
+const somaId = listaCep.reduce((acumulador, item) => {
+    return acumulador + item.id;
+}, 0);
+
+function addCep(cep) {
+    listaCep.push(cep);
+    renderizarCep();
+}
+
+function removerCep(id) {
+    listaCep = listaCep.filter(cep => cep.id !== id);
+    renderizarCep();
+}
+
+
+
+renderizarTarefas();
+
+const btnAdicionar = document.getElementById('btnAdicionar');
+btnAdicionar.addEventListener('click', function () {
+    const cep = document.getElementById('cep').value;
+    if(cep ==""){
+        alert("Por favor digite o cep que será adicionado!!")
+    }
+    else{
+        addTarefa(
+        {
+            id: listaCep.length + 1,
+            cep: cep,
+            feita: false
+        });
+    }
+    
+});
+
+function renderizarCep() {
+    let listaUl = document.getElementById('listaUl');
+    listaUl.innerHTML = '';
+    listaTarefas.map(tarefa => {
+        let li = document.createElement('li');
+        li.classList.add('my-3');
+        li.innerHTML = tarefa.cep + "";
+        li.innerHTML += ` <button type="button" 
+                        class="btn btn-sm btn-danger" 
+                        onclick="removerCep(${cep.id})">
+                        Remover
+                        </button>`;
+        listaUl.appendChild(li);
+    });
+}
+
+renderizarCep();
